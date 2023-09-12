c090cc01eacdb3916641c11f9f4014c305461715
robnoorda <42826244+robnoorda@users.noreply.github.com>
2023-09-11 20:35:50 -0700

    Create HN-thread-lines.js

---
+++ userscripts/HN-thread-lines.js
@@ -0,0 +1,5 @@
+@-moz-document domain("news.ycombinator.com") {
+    .ind {
+    background-image: repeating-linear-gradient(to right, transparent 0px 39px, rgba(204, 204, 204, 1.0) 39px 40px);
+  }
+}
